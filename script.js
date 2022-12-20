262d94855738be0e62ae9ed69c2d630464f61566
Kush Surti <30488380+FrOz3nFir3@users.noreply.github.com>
2022-12-20 16:49:15 +0530

    edge case for regex (no word match)

--- script.js
+++ script.js
@@ -6,6 +6,6 @@ input.addEventListener("input", countWords);
 const wordsRegex = /[a-z]+/gi;
 function countWords() {
   let value = input.value.trim();
-  let wordsLength = value.match(wordsRegex).length;
+  let wordsLength = (value.match(wordsRegex) || []).length;
   wordCount.innerText = String(wordsLength);
 }
