d89ed0b19ee00cc3ef421c7557e5dd3b64b56182
Kush Surti <kushsurti05@gmail.com>
2022-12-20 16:41:48 +0530

    words counts functionality added

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//your code here
+const input = document.getElementById("evaluatedText");
+const wordCount = document.getElementById("wordCount");
+
+input.addEventListener("change", countWords);
+
+const wordsRegex = /[a-z]+/gi;
+function countWords() {
+  let value = input.value.trim();
+  let wordsLength = value.match(wordsRegex).length;
+  wordCount.innerText = String(wordsLength);
+}
